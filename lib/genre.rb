c6bc474059fd99263d2ac602c318c54de03c1a75
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-15 12:57:06 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,32 @@
+class Genre
+  
+  attr_accessor :name, :songs, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    genre = self.new(name)
+    genre.save
+    genre
+  end
+
+  def artists
+    songs.collect{|song| song.artist}.uniq
+  end
+end
